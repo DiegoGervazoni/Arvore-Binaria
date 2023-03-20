e5735fe0c93e4ec02e030e615e43df65a3c8b5e0
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-03-20 11:28:36 -0300

    Implementacao do metodo inserir

---
+++ src/main/java/com/estudos/arvore/ArvoreBinaria.java
@@ -0,0 +1,28 @@
+package com.estudos.arvore;
+
+public class ArvoreBinaria <T extends Comparable <T>> {
+
+    private BinNo<T> raiz;
+
+    public ArvoreBinaria(){
+        this.raiz = null;
+    }
+
+    public void inserir(T conteudo){ //Esse metodo chama o metodo inserir abaixo
+        BinNo<T> novoNo = new BinNo<>(conteudo);
+        this.raiz = inserir(raiz, novoNo);
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
+
+        if(atual == null){ //Se o atual for igual nulo, está na raiz e retorna o novoNo
+            return novoNo;
+        }else if(novoNo.getConteudo().compareTo(atual.getConteudo()) < 0){ //Corre a árvore, indo para esquerda ou direita
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        }else{
+            atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+        }
+        return atual;
+    }
+
+}
