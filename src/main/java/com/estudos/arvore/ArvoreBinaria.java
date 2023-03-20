d39ce1f758f38600e9cf7c9f6b483190dd7d17b4
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-03-20 11:41:54 -0300

    Implementacao do metodo de exibicao

--- src/main/java/com/estudos/arvore/ArvoreBinaria.java
+++ src/main/java/com/estudos/arvore/ArvoreBinaria.java
@@ -1,28 +1,66 @@
 package com.estudos.arvore;
 
-public class ArvoreBinaria <T extends Comparable <T>> {
+public class ArvoreBinaria<T extends Comparable<T>> {
 
     private BinNo<T> raiz;
 
-    public ArvoreBinaria(){
+    public ArvoreBinaria() {
         this.raiz = null;
     }
 
-    public void inserir(T conteudo){ //Esse metodo chama o metodo inserir abaixo
+    public void inserir(T conteudo) { //Esse metodo chama o metodo inserir abaixo
         BinNo<T> novoNo = new BinNo<>(conteudo);
         this.raiz = inserir(raiz, novoNo);
     }
 
-    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
-
-        if(atual == null){ //Se o atual for igual nulo, está na raiz e retorna o novoNo
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo) {
+        if (atual == null) { //Se o atual for igual nulo, está na raiz e retorna o novoNo
             return novoNo;
-        }else if(novoNo.getConteudo().compareTo(atual.getConteudo()) < 0){ //Corre a árvore, indo para esquerda ou direita
+        } else if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) { //Corre a árvore, indo para esquerda ou direita
             atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
-        }else{
+        } else {
             atual.setNoDir(inserir(atual.getNoDir(), novoNo));
         }
         return atual;
+    }
+
+    public void exibirInOrdem() {
+        System.out.println("\nExibindo inOrdem");
+        exibirInOrdem(this.raiz);
+    }
+
+    private void exibirInOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            exibirInOrdem(atual.getNoEsq());
+            System.out.println(atual.getConteudo() + ", ");
+            exibirInOrdem(atual.getNoDir());
+        }
+    }
+
+    public void exibirPosOrdem() {
+        System.out.println("\nExibindo PosOrdem");
+        exibirPosOrdem(this.raiz);
+    }
+
+    private void exibirPosOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            exibirPosOrdem(atual.getNoEsq());
+            exibirPosOrdem(atual.getNoDir());
+            System.out.println(atual.getConteudo() + ", ");
+        }
+    }
+
+    public void exibirPreOrdem() {
+        System.out.println("\nExibindo PreOrdem");
+        exibirPreOrdem(this.raiz);
+    }
+
+    private void exibirPreOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            System.out.println(atual.getConteudo() + ", ");
+            exibirPreOrdem(atual.getNoEsq());
+            exibirPreOrdem(atual.getNoDir());
+        }
     }
 
 }
